2aaa68b37017c9abaa00485a7c0f065747cbdd56
Amey Malekar <admalekar@gmail.com>
2017-12-23 01:08:44 +0530

    Second Commit

--- static/js/loadData.js
+++ static/js/loadData.js
@@ -1,36 +1,35 @@
-$(window).load(function() {
-  // Construct the catalog query string
+
+$(window).load(function initMap() {
+  
+  
+  // Construct the query string
   url = 'https://data.sfgov.org/resource/6a9r-agq8.json?&$$app_token=lRghIp3d7HLYYPhnO7Tvz81R0';
 
-  // Intialize our map
-  var center = new google.maps.LatLng(37.7749 , -122.4194);
+  // Intialize map
+  var center = new google.maps.LatLng(37.7749, -122.4194);
   var mapOptions = {
     zoom: 12,
     center: center
   }
+  
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
- 
- 
- 
-  // Retrieve our data and plot it
-  $.getJSON(url, function(data, textstatus) {
-    console.log(data);
-    $.each(data, function(i, entry) {
 
-     
+  // Retrieve data and plot it
+  $.getJSON(url, function (data, textstatus) {
+    console.log(data);
+    $.each(data, function (i, entry) {
 
       var marker = new google.maps.Marker({
         position: new google.maps.LatLng(entry.latitude,
           entry.longitude),
         map: map,
-        
-        title:"click for more details",
-        
-        
-       
+        title: "click for more details",
+
       });
-      
+
+    });
+
   });
- 
+
 });
-});
+
